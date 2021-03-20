96ef45e6f5ab3134c4f794e70c3f690dfad2bd6b
Apoorva Shukla <apoorva.shukla@publicissapient.com>
2021-03-20 12:24:22 +0530

    Client is calling the server

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const [movies, setMovies] = useState([]);
+  useEffect(() => {
+    console.log('##movies useEffect')
+    fetch("http://server:5000")
+      .then((response) => {
+        console.log('##res', response);
+        response.json();
+      })
+      .then((data) => {
+        console.log(data);
+        setMovies(data.movies)
+      });
+  }, []);
   return (
     <div className="App">
       <h1>This is a movies app using express</h1>
+      {console.log('##movies', movies)}
     </div>
   );
 }
--- server/app.js
+++ server/app.js
@@ -1,9 +1,14 @@
-const express = require('express');
+const express = require("express");
+const cors = require('cors');
 
 const app = express();
 
-const port = process.env.PORT
+const port = process.env.PORT;
 
-app.get('/', (req, res) => res.json({"app": "Movies App", "client": "React"}));
+app.use(cors());
+
+app.get("/", (req, res) =>
+  res.json({ movies: [{ name: "Movie 1" }, { name: "Movie 2" }] })
+);
 
 app.listen(port, () => console.log(`Server is listening on ${port}`));
