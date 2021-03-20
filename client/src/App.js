e24bc8dcba6d2cdad12c3b96cca2dcc13742a958
Apoorva Shukla <apoorva.shukla@publicissapient.com>
2021-03-20 18:16:05 +0530

    Add mongo and create api endpoints

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,76 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "./App.css";
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [movie, setMovie] = useState("");
+  const [release, setRelease] = useState("");
   useEffect(() => {
-    console.log('##movies useEffect')
-    fetch("http://server:5000")
-      .then((response) => {
-        console.log('##res', response);
-        response.json();
-      })
-      .then((data) => {
-        console.log(data);
-        setMovies(data.movies)
-      });
+    (async () => {
+      await fetchMovies();
+    })();
   }, []);
+
+  const submitMovie = async (e) => {
+    e.preventDefault();
+    const newMovie = await axios.post(
+      "http://localhost:5000/api/values/movies",
+      {
+        name: movie,
+        release
+      }
+    );
+    const {
+      data: { data }
+    } = newMovie;
+    setMovies([...movies, data.movie]);
+  };
+
+  const fetchMovies = async () => {
+    const response = await axios.get("http://localhost:5000/api/values/movies");
+    if (
+      response.data &&
+      response.data.data &&
+      response.data.status === "success"
+    ) {
+      const movies = response.data.data.movies.map(({ name, release }) => ({
+        name,
+        release
+      }));
+      setMovies(movies);
+    }
+  };
+
   return (
     <div className="App">
       <h1>This is a movies app using express</h1>
-      {console.log('##movies', movies)}
+      <form onSubmit={submitMovie}>
+        <label>Create a movie: </label>
+        <input
+          name="name"
+          value={movie}
+          onChange={(e) => setMovie(e.target.value)}
+        />
+        <label>Enter year: </label>
+        <input
+          name="year"
+          value={release}
+          onChange={(e) => setRelease(e.target.value)}
+        />
+        <button type="submit">Submit</button>
+      </form>
+      <div className="moviecontainer">
+        {movies.length &&
+          movies.map((movie) => {
+            return (
+              <div key={movie._id}>
+                <span className="movieitem">{movie.name}</span>
+                <span className="movieitem">{movie.release}</span>
+              </div>
+            );
+          })}
+      </div>
     </div>
   );
 }
